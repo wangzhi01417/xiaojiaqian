022f58edd81e68ca94b766a800ba898119275f4f
JasonXin <bibishou@gmail.com>
2013-05-12 22:36:03 +0800

    Change Logo And Cache

---
+++ App/Runtime/Data/_fields/xiaojiaqian.Admin.php
@@ -0,0 +1,2 @@
+<?php
+return array ( 0 => 'id', 1 => 'user_name', 2 => 'password', 3 => 'add_time', 4 => 'last_time', 5 => 'is_del', 6 => 'remark1', 7 => 'remark2', '_autoinc' => true, '_pk' => 'id', ); ?>
