0389203474e5388201826047d464e909d0c2eae3
Ma,Da <motorhust@gmail.com>
2013-05-29 08:45:33 +0800

    items status update & display

--- App/Lib/Action/Home/IndexAction.class.php
+++ App/Lib/Action/Home/IndexAction.class.php
@@ -61,11 +61,11 @@ class IndexAction extends BaseAction {
 		$items_mod=M("Items");//创建单品模型
 		import("@.ORG.FallPage"); // 导入分页类
 		if($id==0)
-		   $where = "is_del = 0 and status = 1 ";
+		   $where = "is_del = 0 and status >= 1 ";
 		else
 		{
 			if($price=="0"){
-				$where = "1=1 and cid = $id and is_del = 0 and status = 1 ";
+				$where = "1=1 and cid = $id and is_del = 0 and status >= 1 ";
 			}else{
 				if($price=="100"){
 					$sql_price="price <= 100";
@@ -78,7 +78,7 @@ class IndexAction extends BaseAction {
 				}else{
 					get_404();
 				}
-				$where = "1=1 and cid = $id and is_del = 0 and status = 1 and ".$sql_price;
+				$where = "1=1 and cid = $id and is_del = 0 and status >= 1 and ".$sql_price;
 			}
 	  }
 		$this->assign("sortby",$sortby);
@@ -86,7 +86,7 @@ class IndexAction extends BaseAction {
 		$count=$items_mod->where($where)->count(); // 查询满足要求的总记录数
 		$Page= new Page($count,20); // 实例化分页类传入总记录数和每页显示的记录数
 		$show =$Page->show(); // 分页显示输出
-		$field = "id,cid,title,price,img,url,uid,sid,likes,comments,add_time,remark1";
+		$field = "id,cid,title,price,img,url,uid,sid,likes,comments,add_time,remark1,status";
 		//$items_list = $items_mod->where($where)->field($field)->order($sql_order)->limit(($Page->firstRow*5).','.$Page->listRows)->select();
         $items_list = $items_mod->where($where)->field($field)->order($sql_order)->limit(($Page->firstRow*1).','.$Page->listRows)->select();
 
